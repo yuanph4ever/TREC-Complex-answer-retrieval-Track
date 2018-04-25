68b329d5e59e358e72558a83e937d254b51bd563
Nithin Sivakumar <nithinsivakumar@outlook.com>
2018-04-25 13:56:03 -0400

    check candidate method on seerver

--- src/main/java/edu/unh/cs980/Main.java
+++ src/main/java/edu/unh/cs980/Main.java
@@ -146,8 +146,7 @@ public class Main {
 			}
 			else
 			{
-				ReadRunFileAndClassify rrfcSecKMeans = new ReadRunFileAndClassify(kmeansRunFile, indexPath, outputPath,
-						"kmeanssection");
+				//ReadRunFileAndClassify rrfcSecKMeans = new ReadRunFileAndClassify(kmeansRunFile, indexPath, outputPath,"kmeanssection");
 			}
 			
 			/**********************************************************************************************************/
